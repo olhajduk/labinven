c2951db671b2388b1f02b3799c10a623ce940abc
Olga Hajduk <hajduk.o.a@gmail.com>
2020-12-15 16:15:42 +0300

    upd webpack add cross-env add sourse maps add eslint

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -2,12 +2,12 @@ console.log('sprite.js')
 
 // import each svg into JS
 function requireAll(r) {
-  r.keys().forEach(r);
+  r.keys().forEach(r)
 }
-requireAll(require.context('./../assets/images/svg', true, /\.svg$/));
+requireAll(require.context('./../assets/images/svg', true, /\.svg$/))
 
 fetch('https://cdn.jsdelivr.net/gh/olhajduk/labinven@gh-pages/sprite.svg').then(res => {
-  return res.text();
+  return res.text()
 }).then(data => {
-  document.getElementById('sprite').innerHTML = data;
-});
+  document.getElementById('sprite').innerHTML = data
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,103 @@
-const path = require('path');
+const path = require('path')
 const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const copyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+    },
+  }]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: './index.js',
-    output: {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core'),
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new SpriteLoaderPlugin({
-            plainSprite: true
-        }),
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        }),
-        new copyWebpackPlugin({
-            patterns: [{
-                from: path.resolve(__dirname, 'src/assets/favicon.ico'),
-                to: path.resolve(__dirname, 'dist')
-            }],
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'bundle.[contenthash].css'
-        })
-    ],
-    module: {
-        rules: [{
-                test: /svg\/.*\.svg$/,
-                loader: 'svg-sprite-loader',
-                options: {
-                    extract: true
-                }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'autoprefixer'
-                                    ]
-                                ]
-                            }
-                        }
-                    },
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
     }
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new SpriteLoaderPlugin({
+      plainSprite: true
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [{
+      test: /svg\/.*\.svg$/,
+      loader: 'svg-sprite-loader',
+      options: {
+        extract: true
+      }
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                [
+                  'autoprefixer'
+                ]
+              ]
+            }
+          }
+        },
+        'sass-loader',
+      ],
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: jsLoaders()
+    }
+    ]
+  }
 }
